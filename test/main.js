83aa61355f5037bd6e94497ef564bf94235a988e
gabriel <furstenheim@gmail.com>
2018-04-22 20:40:57 +0200

    Test circular dependencies

--- test/main.js
+++ test/main.js
@@ -39,4 +39,40 @@ describe('Diffenator', function () {
       expect(files).deep.equal(['test/testDependencyOutOfFolder/test/a.js'])
     })
   })
+  describe('Change common file', function () {
+    let originalFile
+    const filePath = path.join(__dirname, 'testCommonDependency', 'common.js')
+    beforeEach(function () {
+      originalFile = fs.readFileSync(filePath).toString()
+      fs.appendFileSync(filePath, '\n')
+    })
+    afterEach(function () {
+      fs.writeFileSync(filePath, originalFile)
+    })
+    it('Changing with respect to head', async function () {
+      const result = main('test/testCommonDependency/testFolder')
+      expect(result).a('promise')
+      const files = await result
+      expect(files).a('array')
+      expect(files).deep.equal(['test/testCommonDependency/testFolder/a.js', 'test/testCommonDependency/testFolder/b.js'])
+    })
+  })
+  describe('Circular dependency', function () {
+    let originalFile
+    const filePath = path.join(__dirname, 'testCircularDependency', 'a.js')
+    beforeEach(function () {
+      originalFile = fs.readFileSync(filePath).toString()
+      fs.appendFileSync(filePath, '\n')
+    })
+    afterEach(function () {
+      fs.writeFileSync(filePath, originalFile)
+    })
+    it('Changing with respect to head', async function () {
+      const result = main('test/testCircularDependency/test')
+      expect(result).a('promise')
+      const files = await result
+      expect(files).a('array')
+      expect(files).deep.equal(['test/testCircularDependency/test/a.js', 'test/testCircularDependency/test/b.js'])
+    })
+  })
 })
